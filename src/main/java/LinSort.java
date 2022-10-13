33d306df7964d9101f19b8546a476f47c50c81f8
Dandya <Aleksandrmen6@gmail.com>
2022-10-14 00:27:34 +0300

    Completed 7 lab

--- src/main/java/LinSort.java
+++ src/main/java/LinSort.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.ListIterator;
+import java.util.*;
 
 public class LinSort {
 
@@ -63,7 +61,7 @@ public class LinSort {
         }
     }
 
-    public static void LSDSort(Integer[] arr, int begin, int end) throws ArrayIndexOutOfBoundsException {
+    public static void LSDSort(Integer[] arr, int begin, int end) {
         if( begin < 0 || begin > arr.length || end < 0 || end > arr.length || end < begin) {
             throw new ArrayIndexOutOfBoundsException();
         }
@@ -79,5 +77,95 @@ public class LinSort {
 
     public static void LSDSort(Integer[] arr) {
         LSDSort(arr, 0, arr.length);
+    }
+
+    private static void MSDSortR(String[] arr, int begin, int end, int radix) {
+        if(begin == end || end - begin == 1) {
+            return;
+        }
+        myList<String>[] listsForDigits = new myList[11]; // from 0 to 9 and -1
+        for (int i = 0; i < 11; i++) {
+            listsForDigits[i] = new myList<>();
+        }
+        for (int i = begin; i < end; i++) {
+            if (radix < arr[i].length()) {
+                int digit = arr[i].charAt((arr[i].length() - 1) - radix) - '0';
+                if(digit == -3) { // digit is '-'
+                    digit = 10;
+                }
+                listsForDigits[digit].add(arr[i]);
+            } else {
+                listsForDigits[0].add(arr[i]);
+            }
+        }
+        int index = begin;
+        for (ListIterator it = listsForDigits[10].listIterator(listsForDigits[10].size()); it.hasPrevious();) {
+            arr[index] = (String) it.previous();
+            index++;
+        }
+        MSDSortR(arr, begin, index, radix-1);
+        String[] negativeNumbs = new String[index - begin];
+        // TODO: change reverse
+        for(int i = index - 1; i >= begin; i--) {
+            negativeNumbs[index-1-i] = arr[i];
+        }
+        for(int i = begin; i < index; i++) {
+            arr[i] = negativeNumbs[i-begin];
+        }
+        for (int i = 0; i < 10; i++) {
+            int beginIndex = index;
+            for (Iterator it = listsForDigits[i].iterator(); it.hasNext(); ) {
+                arr[index] = (String) it.next();
+                index++;
+            }
+            MSDSortR(arr, beginIndex, index, radix-1);
+        }
+    }
+
+    public static void MSDSort(Integer[] arr, int begin, int end) {
+        if( begin < 0 || begin > arr.length || end < 0 || end > arr.length || end < begin) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        String[] arrStr = new String[end-begin];
+        int max_length = 0;
+        for(int i = begin; i < end; i++) {
+            arrStr[i-begin] = arr[i].toString();
+            if (arrStr[i-begin].length() > max_length) {
+                max_length = arrStr[i-begin].length();
+            }
+        }
+        MSDSortR(arrStr, 0, arrStr.length, max_length-1);
+        for(int i = begin; i < end; i++) {
+            arr[i] = Integer.valueOf(arrStr[i-begin]);
+        }
+    }
+
+    public static void MSDSort(Integer[] arr) {
+        MSDSort(arr, 0, arr.length);
+    }
+
+    public static void CountSort(Integer[] arr, int begin, int end) {
+        if( begin < 0 || begin > arr.length || end < 0 || end > arr.length || end < begin) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        TreeMap<Integer, Integer> tree = new TreeMap<>(Comparator.comparingInt(I -> I));
+        for(int i  = begin; i < end; i++) {
+            if(tree.containsKey(arr[i])) {
+                tree.put(arr[i], tree.get(arr[i])+1);
+            } else {
+                tree.put(arr[i], 1);
+            }
+        }
+        int index = begin;
+        for (Iterator it = tree.keySet().iterator(); it.hasNext();) {
+            Integer key = (Integer) it.next();
+            for(int i = 0; i < tree.get(key); i++) {
+                arr[index++] = key;
+            }
+        }
+    }
+
+    public static void CountSort(Integer[] arr) {
+        CountSort(arr, 0, arr.length);
     }
 }
--- src/test/java/LinSortTest.java
+++ src/test/java/LinSortTest.java
@@ -1,8 +1,6 @@
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -37,6 +35,74 @@ class LinSortTest {
         }
         Arrays.sort(rightArrInt, begin, end);
         LinSort.LSDSort(arrInt, begin, end);
+        System.out.println(begin + " - " + end);
+        System.out.println(Arrays.toString(rightArrInt));
+        System.out.println(Arrays.toString(arrInt));
+        assertArrayEquals(rightArrInt, arrInt);
+    }
+
+    @Test
+    void MSDSort() {
+        System.out.println("\nMSDSort");
+        Random rand = new Random();
+        int size = rand.nextInt(0, 20);
+        Integer[] arrInt = new Integer[size];
+        Integer[] rightArrInt = new Integer[size];
+        for(int i = 0; i < size; i++){
+            arrInt[i] = rand.nextInt();
+            rightArrInt[i] = arrInt[i];
+        }
+        Arrays.sort(rightArrInt);
+        LinSort.MSDSort(arrInt);
+        System.out.println(Arrays.toString(rightArrInt));
+        System.out.println(Arrays.toString(arrInt));
+        assertArrayEquals(rightArrInt, arrInt);
+        //---- sort with bounds
+        size = rand.nextInt(2, 20);
+        int begin = rand.nextInt(0, size-1);
+        int end = rand.nextInt(begin, size-1);
+        arrInt = new Integer[size];
+        rightArrInt = new Integer[size];
+        for(int i = 0; i < size; i++){
+            arrInt[i] = rand.nextInt();
+            rightArrInt[i] = arrInt[i];
+        }
+        Arrays.sort(rightArrInt, begin, end);
+        LinSort.MSDSort(arrInt, begin, end);
+        System.out.println(begin + " - " + end);
+        System.out.println(Arrays.toString(rightArrInt));
+        System.out.println(Arrays.toString(arrInt));
+        assertArrayEquals(rightArrInt, arrInt);
+    }
+
+    @Test
+    void CountSort() {
+        System.out.println("\nCountSort");
+        Random rand = new Random();
+        int size = rand.nextInt(0, 20);
+        Integer[] arrInt = new Integer[size];
+        Integer[] rightArrInt = new Integer[size];
+        for(int i = 0; i < size; i++){
+            arrInt[i] = rand.nextInt();
+            rightArrInt[i] = arrInt[i];
+        }
+        Arrays.sort(rightArrInt);
+        LinSort.CountSort(arrInt);
+        System.out.println(Arrays.toString(rightArrInt));
+        System.out.println(Arrays.toString(arrInt));
+        assertArrayEquals(rightArrInt, arrInt);
+        //---- sort with bounds
+        size = rand.nextInt(2, 20);
+        int begin = rand.nextInt(0, size-1);
+        int end = rand.nextInt(begin, size-1);
+        arrInt = new Integer[size];
+        rightArrInt = new Integer[size];
+        for(int i = 0; i < size; i++){
+            arrInt[i] = rand.nextInt();
+            rightArrInt[i] = arrInt[i];
+        }
+        Arrays.sort(rightArrInt, begin, end);
+        LinSort.CountSort(arrInt, begin, end);
         System.out.println(begin + " - " + end);
         System.out.println(Arrays.toString(rightArrInt));
         System.out.println(Arrays.toString(arrInt));
